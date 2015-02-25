df7ba18e2978a6dd97322967d53e31f048e545aa
Kit <kit@getpantheon.com>
2015-02-25 15:25:10 -0800

    killed the sidebar

--- sites/all/themes/zen/uutheme/templates/page.tpl.php
+++ sites/all/themes/zen/uutheme/templates/page.tpl.php
@@ -159,7 +159,6 @@
       </div></div><!-- /.section, /#navigation -->
     <?php endif; ?>
 
-    <?php print render($page['sidebar_first']); ?>
 
     <?php print render($page['sidebar_second']); ?>
 
